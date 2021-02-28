d705d493cbd9024b41746da2a901d41e43ba2e72
Paul Constant <constantpaul@hotmail.fr>
2021-02-28 18:56:12 +0100

    Correct bench

--- benches/bench_send_events.rs
+++ benches/bench_send_events.rs
@@ -19,16 +19,16 @@ fn bench_one_small_event(c: &mut Criterion) {
     let (main_event_loop, all_event_loops) = pel_create_event_loops();
     // We fetch data in the event loops then launch them.
     // We can then peek inside the loops.
-    let subscriber1_cvar = all_event_loops.subscriber_loop1.get_cvar();
-    let publisher1_cvar = all_event_loops.publisher_loop1.get_cvar();
+    let subscriber_cvar = all_event_loops.subscriber_one_small_event.get_cvar();
+    let publisher_cvar = all_event_loops.publisher_one_small_event.get_cvar();
 
-    pel_launch_every_event_loop_but_main(all_event_loops);
+    pel_launch_event_loops_in_threads(all_event_loops);
     std::thread::spawn(move || pel_run_main_loop_indefinitely(main_event_loop));
 
     c.bench_function("Bench one small event", |b| {
         b.iter(|| {
-            publisher1_cvar.notify();
-            subscriber1_cvar.wait();
+            publisher_cvar.notify();
+            subscriber_cvar.wait();
         })
     });
 }
@@ -37,21 +37,21 @@ fn bench_twenty_small_events(c: &mut Criterion) {
     let (main_event_loop, all_event_loops) = pel_create_event_loops();
     // We fetch data in the event loops then launch them.
     // We can then peek inside the loops.
-    let subscriber2_cvar = all_event_loops.subscriber_loop2.get_cvar();
-    let subscriber3_cvar = all_event_loops.subscriber_loop3.get_cvar();
+    let subscriber1_cvar = all_event_loops.subscriber_many_small_events1.get_cvar();
+    let subscriber2_cvar = all_event_loops.subscriber_many_small_events2.get_cvar();
 
-    let publisher2_cvar = all_event_loops.publisher_loop2.get_cvar();
-    let publisher3_cvar = all_event_loops.publisher_loop3.get_cvar();
+    let publisher1_cvar = all_event_loops.publisher_many_small_events1.get_cvar();
+    let publisher2_cvar = all_event_loops.publisher_many_small_events2.get_cvar();
 
-    pel_launch_every_event_loop_but_main(all_event_loops);
+    pel_launch_event_loops_in_threads(all_event_loops);
     std::thread::spawn(move || pel_run_main_loop_indefinitely(main_event_loop));
 
     c.bench_function("Bench twenty small events", |b| {
         b.iter(|| {
+            publisher1_cvar.notify();
             publisher2_cvar.notify();
-            publisher3_cvar.notify();
+            subscriber1_cvar.wait();
             subscriber2_cvar.wait();
-            subscriber3_cvar.wait();
         })
     });
 }
@@ -60,16 +60,16 @@ fn bench_one_big_event(c: &mut Criterion) {
     let (main_event_loop, all_event_loops) = pel_create_event_loops();
     // We fetch data in the event loops then launch them.
     // We can then peek inside the loops.
-    let subscriber4_cvar = all_event_loops.subscriber_loop4.get_cvar();
-    let publisher4_cvar = all_event_loops.publisher_loop4.get_cvar();
+    let subscriber_cvar = all_event_loops.subscriber_one_big_event.get_cvar();
+    let publisher_cvar = all_event_loops.publisher_one_big_event.get_cvar();
 
-    pel_launch_every_event_loop_but_main(all_event_loops);
+    pel_launch_event_loops_in_threads(all_event_loops);
     std::thread::spawn(move || pel_run_main_loop_indefinitely(main_event_loop));
 
     c.bench_function("Bench one big event", |b| {
         b.iter(|| {
-            publisher4_cvar.notify();
-            subscriber4_cvar.wait();
+            publisher_cvar.notify();
+            subscriber_cvar.wait();
         })
     });
 }
@@ -78,93 +78,93 @@ fn bench_twenty_big_events(c: &mut Criterion) {
     let (main_event_loop, all_event_loops) = pel_create_event_loops();
     // We fetch data in the event loops then launch them.
     // We can then peek inside the loops.
-    let subscriber5_cvar = all_event_loops.subscriber_loop5.get_cvar();
-    let subscriber6_cvar = all_event_loops.subscriber_loop6.get_cvar();
+    let subscriber1_cvar = all_event_loops.subscriber_many_big_events1.get_cvar();
+    let subscriber2_cvar = all_event_loops.subscriber_many_big_events2.get_cvar();
 
-    let publisher5_cvar = all_event_loops.publisher_loop5.get_cvar();
-    let publisher6_cvar = all_event_loops.publisher_loop6.get_cvar();
+    let publisher1_cvar = all_event_loops.publisher_many_big_events1.get_cvar();
+    let publisher2_cvar = all_event_loops.publisher_many_big_events2.get_cvar();
 
-    pel_launch_every_event_loop_but_main(all_event_loops);
+    pel_launch_event_loops_in_threads(all_event_loops);
     std::thread::spawn(move || pel_run_main_loop_indefinitely(main_event_loop));
 
     c.bench_function("Bench twenty big events", |b| {
         b.iter(|| {
-            publisher5_cvar.notify();
-            publisher6_cvar.notify();
-            subscriber5_cvar.wait();
-            subscriber6_cvar.wait();
+            publisher1_cvar.notify();
+            publisher2_cvar.notify();
+            subscriber1_cvar.wait();
+            subscriber2_cvar.wait();
         })
     });
 }
 
 // Boilerplate setup
 
-const N_EVENTS_TO_RECEIVE_BEFORE_NOTIFY: u32 = 10;
+const N_EVENTS_SENT: u32 = 10;
 
 pel::create_event_loops!(
     events: TestEventSmall {},
             TestEventBig {values: Vec<usize>}
 
     active_loops:
-        PublisherLoop1
+        PublisherOneSmallEvent
             {cvar: Arc<PelTestCondvar> = Arc::new(PelTestCondvar::new())}
             publishes (TestEventSmall),
 
-        PublisherLoop2
+        PublisherManySmallEvents1
             {cvar: Arc<PelTestCondvar> = Arc::new(PelTestCondvar::new())}
             publishes (TestEventSmall),
 
-        PublisherLoop3
+        PublisherManySmallEvents2
             {cvar: Arc<PelTestCondvar> = Arc::new(PelTestCondvar::new())}
             publishes (TestEventSmall),
 
-        PublisherLoop4
+        PublisherOneBigEvent
             {cvar: Arc<PelTestCondvar> = Arc::new(PelTestCondvar::new())}
             publishes (TestEventBig),
 
-        PublisherLoop5
+        PublisherManyBigEvents1
             {cvar: Arc<PelTestCondvar> = Arc::new(PelTestCondvar::new())}
             publishes (TestEventBig),
 
-        PublisherLoop6
+        PublisherManyBigEvents2
             {cvar: Arc<PelTestCondvar> = Arc::new(PelTestCondvar::new())}
             publishes (TestEventBig)
 
     reactive_loops:
-        SubscriberLoop1
+        SubscriberOneSmallEvent
             {
                 cvar: Arc<PelTestCondvar> = Arc::new(PelTestCondvar::new())
             }
             subscribes to (TestEventSmall),
 
-        SubscriberLoop2
+        SubscriberManySmallEvents1
             {
                 counter: u32 = 0,
                 cvar: Arc<PelTestCondvar> = Arc::new(PelTestCondvar::new())
             }
         subscribes to (TestEventSmall),
 
-        SubscriberLoop3
+        SubscriberManySmallEvents2
             {
                 counter: u32 = 0,
                 cvar: Arc<PelTestCondvar> = Arc::new(PelTestCondvar::new())
             }
         subscribes to (TestEventSmall),
 
-        SubscriberLoop4
+        SubscriberOneBigEvent
             {
                 cvar: Arc<PelTestCondvar> = Arc::new(PelTestCondvar::new())
             }
             subscribes to (TestEventBig),
 
-        SubscriberLoop5
+        SubscriberManyBigEvents1
             {
                 counter: u32 = 0,
                 cvar: Arc<PelTestCondvar> = Arc::new(PelTestCondvar::new())
             }
         subscribes to (TestEventBig),
 
-        SubscriberLoop6
+        SubscriberManyBigEvents2
             {
                 counter: u32 = 0,
                 cvar: Arc<PelTestCondvar> = Arc::new(PelTestCondvar::new())
@@ -172,176 +172,176 @@ pel::create_event_loops!(
         subscribes to (TestEventBig)
 );
 
-impl MainLoop for PublisherLoop1 {
+impl MainLoop for PublisherOneSmallEvent {
     fn main_loop(&mut self) {
         self.cvar.wait();
         self.publish_test_event_small(TestEventSmall::new());
     }
 }
 
-impl PublisherLoop1 {
+impl PublisherOneSmallEvent {
     pub fn get_cvar(&self) -> Arc<PelTestCondvar> {
         self.cvar.clone()
     }
 }
 
-impl MainLoop for PublisherLoop2 {
+impl MainLoop for PublisherManySmallEvents1 {
     fn main_loop(&mut self) {
         self.cvar.wait();
-        for _ in 0..N_EVENTS_TO_RECEIVE_BEFORE_NOTIFY {
+        for _ in 0..N_EVENTS_SENT {
             self.publish_test_event_small(TestEventSmall::new());
         }
     }
 }
 
-impl PublisherLoop2 {
+impl PublisherManySmallEvents1 {
     pub fn get_cvar(&self) -> Arc<PelTestCondvar> {
         self.cvar.clone()
     }
 }
 
-impl MainLoop for PublisherLoop3 {
+impl MainLoop for PublisherManySmallEvents2 {
     fn main_loop(&mut self) {
         self.cvar.wait();
-        for _ in 0..N_EVENTS_TO_RECEIVE_BEFORE_NOTIFY {
+        for _ in 0..N_EVENTS_SENT {
             self.publish_test_event_small(TestEventSmall::new());
         }
     }
 }
 
-impl PublisherLoop3 {
+impl PublisherManySmallEvents2 {
     pub fn get_cvar(&self) -> Arc<PelTestCondvar> {
         self.cvar.clone()
     }
 }
 
-impl MainLoop for PublisherLoop4 {
+impl MainLoop for PublisherOneBigEvent {
     fn main_loop(&mut self) {
         self.cvar.wait();
         self.publish_test_event_big(TestEventBig::new(vec![4; 100]));
     }
 }
 
-impl PublisherLoop4 {
+impl PublisherOneBigEvent {
     pub fn get_cvar(&self) -> Arc<PelTestCondvar> {
         self.cvar.clone()
     }
 }
 
-impl MainLoop for PublisherLoop5 {
+impl MainLoop for PublisherManyBigEvents1 {
     fn main_loop(&mut self) {
         self.cvar.wait();
-        for _ in 0..N_EVENTS_TO_RECEIVE_BEFORE_NOTIFY {
+        for _ in 0..N_EVENTS_SENT {
             self.publish_test_event_big(TestEventBig::new(vec![5; 100]));
         }
     }
 }
 
-impl PublisherLoop5 {
+impl PublisherManyBigEvents1 {
     pub fn get_cvar(&self) -> Arc<PelTestCondvar> {
         self.cvar.clone()
     }
 }
-impl MainLoop for PublisherLoop6 {
+impl MainLoop for PublisherManyBigEvents2 {
     fn main_loop(&mut self) {
         self.cvar.wait();
-        for _ in 0..N_EVENTS_TO_RECEIVE_BEFORE_NOTIFY {
+        for _ in 0..N_EVENTS_SENT {
             self.publish_test_event_big(TestEventBig::new(vec![6; 100]));
         }
     }
 }
 
-impl PublisherLoop6 {
+impl PublisherManyBigEvents2 {
     pub fn get_cvar(&self) -> Arc<PelTestCondvar> {
         self.cvar.clone()
     }
 }
 
-impl SubscriberLoop1EventHandlers for SubscriberLoop1 {
+impl SubscriberOneSmallEventEventHandlers for SubscriberOneSmallEvent {
     fn on_test_event_small(&mut self, _event: TestEventSmall) {
         self.cvar.notify();
     }
 }
 
-impl SubscriberLoop1 {
+impl SubscriberOneSmallEvent {
     pub fn get_cvar(&self) -> Arc<PelTestCondvar> {
         self.cvar.clone()
     }
 }
 
-impl SubscriberLoop2EventHandlers for SubscriberLoop2 {
+impl SubscriberManySmallEvents1EventHandlers for SubscriberManySmallEvents1 {
     fn on_test_event_small(&mut self, _event: TestEventSmall) {
         self.counter += 1;
-        if self.counter == N_EVENTS_TO_RECEIVE_BEFORE_NOTIFY {
+        if self.counter == N_EVENTS_SENT * 2 {
             self.cvar.notify();
             self.counter = 0;
         }
     }
 }
 
-impl SubscriberLoop2 {
+impl SubscriberManySmallEvents1 {
     pub fn get_cvar(&self) -> Arc<PelTestCondvar> {
         self.cvar.clone()
     }
 }
 
-impl SubscriberLoop3EventHandlers for SubscriberLoop3 {
+impl SubscriberManySmallEvents2EventHandlers for SubscriberManySmallEvents2 {
     fn on_test_event_small(&mut self, _event: TestEventSmall) {
         self.counter += 1;
-        if self.counter == N_EVENTS_TO_RECEIVE_BEFORE_NOTIFY {
+        if self.counter == N_EVENTS_SENT * 2 {
             self.cvar.notify();
             self.counter = 0;
         }
     }
 }
 
-impl SubscriberLoop3 {
+impl SubscriberManySmallEvents2 {
     pub fn get_cvar(&self) -> Arc<PelTestCondvar> {
         self.cvar.clone()
     }
 }
 
-impl SubscriberLoop4EventHandlers for SubscriberLoop4 {
+impl SubscriberOneBigEventEventHandlers for SubscriberOneBigEvent {
     fn on_test_event_big(&mut self, event: TestEventBig) {
         black_box(event);
         self.cvar.notify();
     }
 }
 
-impl SubscriberLoop4 {
+impl SubscriberOneBigEvent {
     pub fn get_cvar(&self) -> Arc<PelTestCondvar> {
         self.cvar.clone()
     }
 }
 
-impl SubscriberLoop5EventHandlers for SubscriberLoop5 {
+impl SubscriberManyBigEvents1EventHandlers for SubscriberManyBigEvents1 {
     fn on_test_event_big(&mut self, event: TestEventBig) {
         black_box(event);
         self.counter += 1;
-        if self.counter == N_EVENTS_TO_RECEIVE_BEFORE_NOTIFY {
+        if self.counter == N_EVENTS_SENT * 2 {
             self.cvar.notify();
             self.counter = 0;
         }
     }
 }
 
-impl SubscriberLoop5 {
+impl SubscriberManyBigEvents1 {
     pub fn get_cvar(&self) -> Arc<PelTestCondvar> {
         self.cvar.clone()
     }
 }
-impl SubscriberLoop6EventHandlers for SubscriberLoop6 {
+impl SubscriberManyBigEvents2EventHandlers for SubscriberManyBigEvents2 {
     fn on_test_event_big(&mut self, event: TestEventBig) {
         black_box(event);
         self.counter += 1;
-        if self.counter == N_EVENTS_TO_RECEIVE_BEFORE_NOTIFY {
+        if self.counter == N_EVENTS_SENT * 2 {
             self.cvar.notify();
             self.counter = 0;
         }
     }
 }
 
-impl SubscriberLoop6 {
+impl SubscriberManyBigEvents2 {
     pub fn get_cvar(&self) -> Arc<PelTestCondvar> {
         self.cvar.clone()
     }
